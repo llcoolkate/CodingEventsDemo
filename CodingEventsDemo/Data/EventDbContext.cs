fabafb0c624e76f447c2f9e99f8ad66304883ccc
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-11-30 18:45:19 -0600

    store

---
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using CodingEventsDemo.Models;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options) 
+        {
+        }
+    }
+}
